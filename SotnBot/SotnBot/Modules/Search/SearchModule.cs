3b9baa1e563acca84d6ec476c9b0c7fb8cf25ea7
Malven <marcusalven@gmail.com>
2016-04-05 18:39:09 +0200

    Added startup code and an initial search module

---
+++ SotnBot/SotnBot/Modules/Search/SearchModule.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SotnBot.Modules.Search
+{
+    internal class SearchModule : IModule
+    {
+        private ModuleManager _manager;
+        private DiscordClient _client;
+        private bool _isRunning;
+
+        void IModule.Install(ModuleManager manager)
+        {
+            _manager = manager;
+            _client = manager.Client;
+
+            manager.CreateCommands("search", group =>
+            {
+                group.CreateCommand("imdb")
+                .Description("Searches IMDB.")
+                .Parameter("value", ParameterType.Required)
+                .Do(async e =>
+                {
+                    e.Channel.SendIsTyping();
+                    string result;
+                    var movie = ImdbScraper.ImdbScrape(e.Args[0], true);
+                    if (movie.Status) result = movie.ToString();
+                    else result = "Couldn't find that movie.";
+                    await e.Channel.SendMessage(result);
+                });
+            });
+        }
+    }
+}
---
+++ SotnBot/SotnBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Modules;
+using SotnBot.Modules.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SotnBot
+{
+    class Program
+    {
+        private DiscordClient _client;
+
+        static void Main(string[] args) => new Program().Start(args);
+
+        private void Start(string[] args)
+        {
+            _client = new DiscordClient(x =>
+            {
+                x.AppName = "S.O.T.N-BOT";
+            })
+            .UsingCommands(x => {
+                x.HelpMode = HelpMode.Public;
+            })
+            .UsingModules();
+
+            _client.AddModule<SearchModule>("Search", ModuleFilter.None);
+
+            _client.ExecuteAndWait(async () =>
+            {
+                await _client.Connect("dahil82@gmail.com", "d6qkwa52");
+                _client.SetGame("sotn.malven.se");
+            });
+        }
+    }
+}
